079472090bdb8a141e3027a72bbad00448713c69
corey98x2 <corey98x2@gmail.com>
2013-12-08 16:57:10 -0800

    Price Update

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ActiveRecord::Base
+  validates :name, :image_url, :place, :special, :user, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :rating, numericality: {greater_than_or_equal_to: 0.1}
 end
+
+
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "item attributes must not be empty" do
+    item = Item.new
+    assert item.invalid?
+    assert item.errors[:name].any?
+    assert item.errors[:image_url].any?
+    assert item.errors[:place].any?
+    assert item.errors[:special].any?
+    assert item.errors[:user].any?
+    assert item.errors[:price].any?
+    assert item.errors[:rating].any?
+  end
+
+  test "item price must be positive" do
+    item = Item.new(name:     "steak",
+                    image_url: "steak.jpg",
+                    place: "mcdonalds",
+                    special: "no",
+                    user: "corey",
+                    rating: 4,
+                    date: Time.now )
+
+    item.price = -1
+    assert item.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      item.errors[:price]
+
+    item.price = 0
+    assert item.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      item.errors[:price]
+
+    item.price = 1
+    assert item.valid?
+  end
 end
+
+
